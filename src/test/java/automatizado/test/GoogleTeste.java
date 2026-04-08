756f7cf94a7a32b03bb5a89fa1ca487c662cdc28
Guilherme Oliveira <guilhermeosilva2406@gmail.com>
2026-04-08 10:54:03 -0300

    2026/04/08

---
+++ src/test/java/automatizado/test/GoogleTeste.java
@@ -0,0 +1,24 @@
+package automatizado.test;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleTeste {
+    private WebDriver driver;
+    private final String URL_BASE = "https://www.google.com/";
+    private final String CAMINHO_DRIVE = "C:\\Users\\guilh\\Documents\\automacao-java\\automatizado\\src\\test\\java\\automatizado\\resource\\chromedriver.exe";
+
+    private void iniciar() {
+        System.setProperty("webdriver.chrome.driver", CAMINHO_DRIVE);
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(URL_BASE);
+    }
+
+    @Test
+    public void devePesquisarNoGoogle() {
+        iniciar();
+
+    }
+}
